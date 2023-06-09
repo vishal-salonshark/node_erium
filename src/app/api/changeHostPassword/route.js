b1b38b7c0805214250c233a58c53f1ded2f27240
vishal0298 <vishalpawar0298@gmail.com>
2023-06-09 17:58:49 +0530

    June9 commit

--- src/app/api/changeHostPassword/route.js
+++ src/app/api/changeHostPassword/route.js
@@ -1,35 +1,44 @@
-import wifi from "node-wifi";
-
+const { exec } = require('child_process');
+const readline = require('readline');
 // Connect to WiFi network
-function connectToWiFi(ssid, password) {
-  return new Promise((resolve, reject) => {
-    wifi.connect({ ssid, password }, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+// function changeHostPassword(ssid, password) {
+//   return new Promise((resolve, reject) => {
+//     wifi.connect({ ssid, password }, (error) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// }
 
 export async function POST(req) {
   try {
-    // Initialize wifi module
-    wifi.init({
-      iface: null, // Network interface to use (optional)
+    
+    const { username, password } = req.body;
+
+    const command = `echo "${username}:${password}" | sudo chpasswd`;
+  
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        // return res.status(500).json({ error: 'An error occurred while changing the password.' });
+        return new Response(JSON.stringify({ error: 'An error occurred while changing the password.'}), { status: 500 });
+      } else if (stderr) {
+        console.error(`Command execution failed: ${stderr}`);
+        // return res.status(500).json({ error: 'An error occurred while changing the password.' });
+        return new Response(JSON.stringify({ error: 'An error occurred while changing the password.'}), { status: 500 });
+      } else {
+        console.log(`Password for ${username} has been changed successfully.`);
+        return new Response(JSON.stringify({ message: 'Password changed successfully.' }), { status: 200 });
+      }
     });
 
-    const { ssid, password } = await req.json();
-
-    const result = await connectToWiFi(ssid, password);
-    console.log(result);
-    // res.json({ message: result });
-
-    return new Response(
-      JSON.stringify({ message: "Successfully connected to WiFi." }),
-      { status: 200 }
-    );
+    // return new Response(
+    //   JSON.stringify({ message: "Successfully connected to WiFi." }),
+    //   { status: 200 }
+    // );
   } catch (error) {
     return new Response(JSON.stringify(error.message), { status: 500 });
   }
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,6 +5,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 const page = () => {
   const { data: session } = useSession()
+  console.log(session)
   if(session) {
   return (
     <div>
